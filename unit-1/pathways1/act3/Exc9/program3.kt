d3fb9ee2153de4a3175bdd092e930e839a981e29
Sanjiida <107598232+Sanjiida@users.noreply.github.com>
2022-08-06 16:46:24 +0600

    Create program3.kt

---
+++ unit-1/pathways1/act3/Exc9/program3.kt
@@ -0,0 +1,16 @@
+//code:
+fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+//output:
+
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
